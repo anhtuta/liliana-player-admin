e3f879096d5236e3e4ec61fdc8f9d27e50a9b66f
Ta Anh Tu <tu.ta.anh@nextop.asia>
2022-05-27 16:35:21 +0700

    Revert "Add crypto-js"
    This reverts commit 47098f81f62166d3405134d54ad25f112402ccca.

--- src/service/crypto.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-export const sha256Hex = (message) => {
-  return CryptoJS.SHA256(message).toString(CryptoJS.enc.Hex);
-};
-
-export const hmacSha512Hex = (message, key) => {
-  return CryptoJS.HmacSHA512(message, key).toString(CryptoJS.enc.Hex);
-};
