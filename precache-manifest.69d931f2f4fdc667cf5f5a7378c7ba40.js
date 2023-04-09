d16bc5a74daddd38093201e11ac3c132099b8124
Ta Anh Tu <tu.taanh@ascendcorp.com>
2023-04-09 13:38:45 +0700

    Updates

---
+++ precache-manifest.69d931f2f4fdc667cf5f5a7378c7ba40.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703bef94c630df0e3c21cc49f9e21465",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de9e5e8623cfb2f4b2c9",
+    "url": "./static/css/2.d6823a40.chunk.css"
+  },
+  {
+    "revision": "e1ae8fbaa9c802d56b87",
+    "url": "./static/css/3.41a630f7.chunk.css"
+  },
+  {
+    "revision": "a827f48afef5d3e7c5c2",
+    "url": "./static/css/4.66b51887.chunk.css"
+  },
+  {
+    "revision": "972f4e65595d1282e610",
+    "url": "./static/css/5.eafbe1c4.chunk.css"
+  },
+  {
+    "revision": "394e0c76f8ceaee9b65d",
+    "url": "./static/css/6.94b58b15.chunk.css"
+  },
+  {
+    "revision": "96b57367ea5fdd4a9208",
+    "url": "./static/css/main.19da8268.chunk.css"
+  },
+  {
+    "revision": "de9e5e8623cfb2f4b2c9",
+    "url": "./static/js/2.7944422f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.7944422f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ae8fbaa9c802d56b87",
+    "url": "./static/js/3.2116a8a3.chunk.js"
+  },
+  {
+    "revision": "5fa66bbcfb2db9241dd0d202733345f3",
+    "url": "./static/js/3.2116a8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a827f48afef5d3e7c5c2",
+    "url": "./static/js/4.796a3ed1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/4.796a3ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972f4e65595d1282e610",
+    "url": "./static/js/5.872a0383.chunk.js"
+  },
+  {
+    "revision": "394e0c76f8ceaee9b65d",
+    "url": "./static/js/6.520e46ff.chunk.js"
+  },
+  {
+    "revision": "b37447c7e744ae41466a",
+    "url": "./static/js/7.6b124594.chunk.js"
+  },
+  {
+    "revision": "142e5ca8a07e465923fb",
+    "url": "./static/js/8.7790d52a.chunk.js"
+  },
+  {
+    "revision": "6ce1e3967dcf487bfd3f",
+    "url": "./static/js/9.86577d5d.chunk.js"
+  },
+  {
+    "revision": "96b57367ea5fdd4a9208",
+    "url": "./static/js/main.3669d3e8.chunk.js"
+  },
+  {
+    "revision": "1d67851d192c12b4452b",
+    "url": "./static/js/runtime-main.f36c2a89.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8ef92b783453520eaa16585763cb2e35",
+    "url": "./static/media/icon-loading.8ef92b78.svg"
+  }
+]);
---
+++ static/js/8.7790d52a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpliliana-player-admin"]=this["webpackJsonpliliana-player-admin"]||[]).push([[8],{187:function(e,a,n){"use strict";n.r(a);var t=n(16),i=n(17),r=n(30),l=n(29),u=n(0),c=n.n(u),p=function(e){Object(r.a)(n,e);var a=Object(l.a)(n);function n(){return Object(t.a)(this,n),a.apply(this,arguments)}return Object(i.a)(n,[{key:"render",value:function(){return c.a.createElement("div",null,c.a.createElement("h2",null,"Liliana Player admin!"))}}]),n}(u.PureComponent);a.default=p}}]);
+//# sourceMappingURL=8.7790d52a.chunk.js.map
